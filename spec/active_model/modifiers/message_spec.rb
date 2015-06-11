b3ab65a455a80934c56dd19902a6b87eb767466c
Jake Yesbeck <jake@apartmentlist.com>
2015-06-10 23:25:04 -0700

    Modifier spec - custom messaging
    Test that when adding `message: 'some message'` the library responds
      correctly and lets the user set their desired error messaging

---
+++ spec/active_model/modifiers/message_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../../../lib/validates_subset'
+
+class TestMessage
+  include ActiveModel::Validations
+
+  attr_accessor :foo
+
+  validates_subset :foo, [1, 2, 3], message: 'Oh no spaghettios'
+end
+
+describe 'custom messaging' do
+  subject { sub = TestMessage.new; sub.foo = value; sub }
+
+  context 'value is a valid subset' do
+    let(:value) { [1] }
+
+    specify do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'value is not a valid subset' do
+    let(:value) { %i[a b c] }
+
+    specify do
+      expect(subject).to_not be_valid
+    end
+
+    it 'sets the custom error message' do
+      subject.validate
+      expect(subject.errors[:foo].first).to match(/Oh no spaghettios/)
+    end
+  end
+
+  context 'value is not an array' do
+    let(:value) { 'bar' }
+
+    specify do
+      expect(subject).to_not be_valid
+    end
+
+    it 'sets the custom error message' do
+      subject.validate
+      expect(subject.errors[:foo].first).to match(/Oh no spaghettios/)
+    end
+  end
+end
